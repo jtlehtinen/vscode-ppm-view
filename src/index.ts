45b786937a85b5d6495f9089bd33e0313487e459
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-01 17:50:20 +0200

    display something in the editor view

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode'
 
-function generateHTML() {
+function generateHTML(_uri: vscode.Uri) {
   return `
 <!DOCTYPE html>
 <html lang="en">
@@ -8,10 +8,61 @@ function generateHTML() {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PPM View</title>
+  <title>PPM</title>
+
+  <style>
+    * {
+      box-sizing: border-box;
+    }
+
+    html {
+      height: 100%;
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  </style>
 </head>
 <body>
-  <h1>Hello PPM</h1>
+  <canvas id="canvas"></canvas>
+
+  <script>
+    const width = 256
+    const height = 256
+
+    const canvas = document.getElementById('canvas')
+    canvas.width = width
+    canvas.height = height
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
+    const context = canvas.getContext('2d')
+    const imageData = context.createImageData(width, height)
+    const data = imageData.data
+
+    let idx = 0
+    for (let y = 0; y < height; ++y) {
+      for (let x = 0; x < width; ++x) {
+        const r = x & 0xff
+        const g = y & 0xff
+        const b = 0
+        const a = 255
+
+        data[idx++] = r
+        data[idx++] = g
+        data[idx++] = b
+        data[idx++] = a
+      }
+    }
+
+    context.putImageData(imageData, 0, 0)
+  </script>
 </body>
 </html>
 `
@@ -34,7 +85,7 @@ class PPMDocument implements vscode.CustomDocument {
 // WebviewCollection taken from
 // https://github.com/microsoft/vscode-extension-samples/blob/main/custom-editor-sample/src/pawDrawEditor.ts
 class WebviewCollection {
-  private readonly _webviews = new Set<{ readonly resource: string; readonly webviewPanel: vscode.WebviewPanel; }>()
+  private readonly _webviews = new Set<{ readonly resource: string; readonly webviewPanel: vscode.WebviewPanel }>()
 
   // Get all known webviews for a given uri.
   public *get(uri: vscode.Uri): Iterable<vscode.WebviewPanel> {
@@ -64,15 +115,15 @@ class PPMProvider implements vscode.CustomReadonlyEditorProvider<PPMDocument> {
   }
 
   async resolveCustomEditor(document: PPMDocument, webviewPanel: vscode.WebviewPanel, _token: vscode.CancellationToken): Promise<void> {
-    webviewPanel.webview.options = { enableScripts: true, }
-    webviewPanel.webview.html = generateHTML()
+    webviewPanel.webview.options = { enableScripts: true }
+    webviewPanel.webview.html = generateHTML(document.uri)
     this.webviews.add(document.uri, webviewPanel)
   }
 }
 
 export function activate(context: vscode.ExtensionContext) {
   const provider = new PPMProvider(context)
-  const options = { supportsMultipleEditorsPerDocument: false, }
+  const options = { supportsMultipleEditorsPerDocument: false }
   const disposable = vscode.window.registerCustomEditorProvider('vscode-ppm-view.ppm-view', provider, options)
   context.subscriptions.push(disposable)
 
